ed0555f0b076c70a1738023648a39f599028e63a
juan lugo <jlugo@juan-lugos-macbook-4.local>
2012-04-02 15:48:37 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omblog::Application.config.secret_token = 'a69af60f1ac9cb0111dc88cb1cf51ecbf846b9f1d603a4ca16e3cf30e263a790cbf82f20ce86e9045763905a5abad16c066efca391ab3240aff5ddd0a7245962'
